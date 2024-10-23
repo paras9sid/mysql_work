f2aed2534cf0b3975b15e43596b62e3b26b9c67a
Sidharth Jain <sidharthjainp9@gmail.com>
2024-10-23 20:53:46 +0530

    creating/using/deleting database

---
+++ creating db & tables/creating_db.sql
@@ -0,0 +1,17 @@
+-- FOR COMMENTS DOUBLE DASHES ' -- '
+
+-- TO SEE ALL DATABSES AVAILABLE
+SHOW databases;
+
+-- creating databases
+CREATE DATABASE new_db;
+
+-- to use/select db to work on
+USE new_db;
+
+-- dropping/delete db
+DROP database new_db;
+
+
+
+
